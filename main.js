3b5459691d10bd96fa5a87e5170566936a4d4a9e
UmaMadhav Eedara <umamadhaveedara@gmail.com>
2023-12-20 15:18:08 +0530

    Nav  setup

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-// add JS
+const mobileNav = document.querySelector(".hamburger");
+const navbar = document.querySelector(".menubar");
+
+const toggleNav = () => {
+  navbar.classList.toggle("active");
+  mobileNav.classList.toggle("hamburger-active");
+};
+mobileNav.addEventListener("click", () => toggleNav());
